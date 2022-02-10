2f6ed3b9c383c18a668dcdbd860900aa6dd2fe4d
Lorenzo Spadaro <arcap@DESKTOP-IDPHOJJ>
2022-02-10 10:15:06 +0100

    creazione classe test Tribuna e aggiornamento classi test 2° Iterazione

---
+++ src/test/java/com/Tifoticket/TestTribuna.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Tifoticket;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author arcap
+ */
+public class TestTribuna {
+     Tribuna se;
+     @BeforeEach
+     public void init(){
+          se=new Tribuna("Tribuna Est",10);
+     }
+     
+     @Test
+     public void testSceltaPosto(){
+          //SCELTA CORRETTA
+          try {
+               assertNotNull(se.sceltaPosto(1, 10));
+          } catch (Exception ex) {
+               ex.getMessage();
+          }
+          //FALLISCE PERCHE' HO UNA FILA DA 10 POSTI
+          try{
+               se.sceltaPosto(1,11);
+               fail();
+          }
+          catch(Exception e){
+               System.err.println(e.getMessage());
+          }
+     }
+     
+}
